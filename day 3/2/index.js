8cca3a73126306ea1fb84546e665b20acc1aa0c5
kevinveld2001 <kevinveldscholte@gmail.com>
2020-12-03 08:40:23 +0100

    day 3.2 cleanup

--- day 3/2/index.js
+++ day 3/2/index.js
@@ -20,88 +20,29 @@ var treecount2 = 0;
 var treecount3 = 0;
 var treecount4 = 0;
 console.log('horizon '+horizon);
-while(meheight < height){
-    
-    if(meright >= horizon){
-        meright -= horizon;
-    }
-    
-    if(data[meheight][meright] === "#"){
-        treecount++;
-    }
-    meheight += 1;
-    meright += 1;
-    
-    
-}
-console.log(treecount);
-meheight = 0;
-meright = 0;
-while(meheight < height){
-    
-    if(meright >= horizon){
-        meright -= horizon;
-    }
-    
-    if(data[meheight][meright] === "#"){
-        treecount1++;
-    }
-    meheight += 1;
-    meright += 3;
-    
-    
-}
-console.log(treecount1);
-meheight = 0;
-meright = 0;
-while(meheight < height){
-    
-    if(meright >= horizon){
-        meright -= horizon;
-    }
-    
-    if(data[meheight][meright] === "#"){
-        treecount2++;
-    }
-    meheight += 1;
-    meright += 5;
-    
-    
-}
-console.log(treecount2);
-meheight = 0;
-meright = 0;
-while(meheight < height){
-    
-    if(meright >= horizon){
-        meright -= horizon;
-    }
-    
-    if(data[meheight][meright] === "#"){
-        treecount3++;
-    }
-    meheight += 1;
-    meright += 7;
-    
-    
-}
-console.log(treecount3);
-meheight = 0;
-meright = 0;
-while(meheight < height){
-    
-    if(meright >= horizon){
-        meright -= horizon;
-    }
-    
-    if(data[meheight][meright] === "#"){
-        treecount4++;
-    }
-    meheight += 2;
-    meright += 1;
-    
-    
-}
-console.log(treecount4);
 
-console.log("* = "+treecount * treecount1 * treecount2* treecount3 *treecount4)
+function calcslope (y,x) {
+     meheight = 0;
+     meright = 0;
+
+     treecount = 0;
+    while(meheight < height){
+    
+        if(meright >= horizon){
+            meright -= horizon;
+        }
+        
+        if(data[meheight][meright] === "#"){
+            treecount++;
+        }
+        meheight += y;
+        meright += x;
+        
+        
+    }
+    return treecount;
+}
+
+
+
+console.log("* = "+calcslope(1,1) * calcslope(1,3) * calcslope(1,5)* calcslope(1,7) *calcslope(2,1));
